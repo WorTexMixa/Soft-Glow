893cd06c14601a5b3becc077a516148805ed258d
WorTexMixa <mihajackson2@gmail.com>
2026-04-30 23:08:34 +0300

    Add navigation menu to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,8 +2,16 @@ import './Header.css'
 
 function Header() {
     return (
-        <header>
-            <h1>Soft Glow</h1>
+        <header className="header">
+            <div className="logo">Soft Glow</div>
+
+            <nav className="nav">
+            <a href="#">Головна</a>
+            <a href="#">Послуги</a>
+            <a href="#">Майстри</a>
+            <a href="#">Запис</a>
+            <a href="#">Контакти</a>
+            </nav>
         </header>
     )
 }
